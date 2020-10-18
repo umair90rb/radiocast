7b2e77b06b314a77fb864d2175f710e457a11e9f
Umair <umair90rb@gmail.com>
2020-10-18 20:19:29 +0500

    before first testing

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -10,8 +10,74 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index){
+              return CurvedListItem(
+                title: "abc",
+              );
+            }
+        )
+      ),
+    );
+  }
+}
+
+class CurvedListItem extends StatelessWidget {
+  const CurvedListItem({
+    this.title,
+    this.time,
+    this.icon,
+    this.people,
+    this.color,
+    this.nextColor,
+  });
+
+  final String title;
+  final String time;
+  final String people;
+  final IconData icon;
+  final Color color;
+  final Color nextColor;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Text('Home'),
+      color: nextColor,
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(80.0),
+          ),
+        ),
+        padding: const EdgeInsets.only(
+          left: 32,
+          top: 80.0,
+          bottom: 50,
+        ),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                time,
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              ),
+              Row(),
+            ]),
+      ),
     );
   }
 }
